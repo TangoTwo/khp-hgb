8605d66b02a0442c74bb56831414ee02fd824393
SydoxX <konstantin@papesh.at>
2018-04-02 14:13:31 +0200

    "Done stuff"

---
+++ ss18/ex2/WordCounter.pas
@@ -0,0 +1,39 @@
+(* WordCounter:                                  HDO, 2003-02-28 *)
+(* -----------                                                   *)
+(* Template for programs that count words in text files.         *)
+(*===============================================================*)
+PROGRAM WordCounter;
+
+  USES
+    Timer, WordReader, ModHashTableLinearProbing;
+
+VAR
+    c, maxC : integer;
+    mostFreqWord : string;
+    w: Word;
+    n: LONGINT;
+
+BEGIN (*WordCounter*)
+    WriteLn('WordCounter:');
+    OpenFile('Kafka.txt', toLower);
+  StartTimer;
+  n := 0;
+  maxC := 0;
+  ReadWord(w);
+  WHILE w <> '' DO BEGIN
+      n := n + 1;
+    c := addOrInc(w);
+    if c > maxC then begin
+            maxC := c;
+            mostFreqWord := w;
+    end;
+    writeln('THE');
+    ReadWord(w);
+  END; (*WHILE*)
+  StopTimer;
+  CloseFile;
+  WriteLn('number of words: ', n);
+  WriteLn('elapsed time:    ', ElapsedTime);
+  writeLn('Most frequent word ', mostFreqWord, ' ', maxC);
+
+END. (*WordCounter*)
