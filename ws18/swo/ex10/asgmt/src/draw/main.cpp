d421c2cf34ed66182d7460e3ce04995ba99198d8
Konstantin Papesh <konstantin@papesh.at>
2019-03-18 18:16:47 +0100

    did something with ex10

--- ws18/swo/ex10/asgmt/src/draw/main.cpp
+++ ws18/swo/ex10/asgmt/src/draw/main.cpp
@@ -1,4 +1,11 @@
 //
-// Created by khp on 20.01.19.
+// Created by root on 1/21/19.
 //
 
+#include <ml5/ml5.h>
+#include "DrawApplication.h"
+
+int main(int argc, char *argv[]) {
+    DrawApplication{}.run(argc, argv);
+    return 0;
+}
